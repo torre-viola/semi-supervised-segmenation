95332ad6e46b14e9db3644dbbad205af9179eed6
Torre Viola <torre.l.viola@lmco.com>
2022-11-26 19:42:00 -0800

    finalizes data setup script

--- scripts/generate_csvs.py
+++ scripts/generate_csvs.py
@@ -1,13 +1,83 @@
 import csv
 import os
 from PIL import Image
+import random
+
+
+"""
+READ ME
+
+The purpose of this script is to save images as a valid PyMIC file type (png) and 
+create the csvs for use in the dataset section of the PyMIC training configs. If 
+you don't want to make your own CSVs, you can use the ones found in 
+src/ssl_entropy_minimization/config/data/. Just comment out the "with open" loop
+
+This script anticipates the images and their labels are stored in a directory structured
+as follows:
+
+Note: the train.txt and valid.txt are specifically called out below because they aren't 
+images or labels and they are necessary for the code. The images and labels should be 
+placed appropriately in the folders below
+
+main_directory
+    |
+    ---- "images"
+    |       |
+    |       --'AppleA/'
+    |       |
+    |       --'AppleB/'
+    |       |
+    |       -- 'Pear/'
+    |       |
+    |       --'Peach/'
+    |
+    ---- "labels"
+            |
+            --'AppleA/'
+            |   * valid.txt 
+            |   * train.txt
+            |
+            --'AppleB/'
+            |
+            -- 'Pear/'
+            |
+            --'Peach/'
+
+"""
 # assign directory
-main_directory = '../fruit_data/'
-fruits = ['AppleB/', 'Pear/', 'Peach/']
- 
-# iterate over the different fruit classes
+main_directory = 'fruit_data/'
+
+"""
+## AppleA Pre-processing
+# Original AppleA data is handled differently than the other three classes which is 
+# why it has its own section.
+# This takes a while - would recommend leaving it commented unless you need it
+
+appleA_dir = main_directory + 'images/AppleA'
+for filename in os.listdir(appleA_dir):
+        f = os.path.join(appleA_dir, filename)
+        pathname, _ = os.path.splitext(f)
+        pathname = pathname.replace("IMG_0", "")
+        
+        img = Image.open(f)
+        new_name = pathname+".png"
+        img.save(pathname+".png")
+"""
+with open(main_directory+'/labels/AppleA/train.txt', 'r') as f:
+    appleA_train_raw = f.readlines()
+appleA_train = [i.replace("IMG_0", "").replace("\n", "") for i in appleA_train_raw]
+
+with open(main_directory+'/labels/AppleA/train.txt', 'r') as f:
+    appleA_valid_raw = f.readlines()
+appleA_valid = [i.replace("IMG_0", "").replace(".JPG\n", ".png") for i in appleA_valid_raw]
+print("Finished AppleA Processing, moving on")
+
+# iterate over the remaining fruit classes
 file_list = []
+fruits = ['AppleB/', 'Pear/', 'Peach/']
 for fruit in fruits:
+    print(f"Beginning processing for {fruit} directory")
+    fruit_files = []
     directory = main_directory + 'images/' + fruit 
     for filename in os.listdir(directory):
         f = os.path.join(directory, filename)
@@ -24,8 +94,24 @@ for fruit in fruits:
         f = os.path.join(directory, filename)
         if os.path.isfile(f) and f.endswith(".png"):
             train_pair = ['images/'+fruit+filename, 'labels/'+fruit+filename]
-            file_list.append(train_pair)
+            fruit_files.append(train_pair)
 
-with open('image_train_pngs.csv', 'w') as f:
+    file_list.append(fruit_files)
+
+print("Beginning CSV file Creation")
+with open('image_train.csv', 'w') as f:
     write = csv.writer(f)
-    write.writerows(file_list)
+    for fruit_list in file_list:
+        write.writerows(fruit_list[2:-5])
+    write.writerows(appleA_train[:-15])
+with open('image_valid.csv', 'w') as f:
+    write = csv.writer(f)
+    for fruit_list in file_list:
+        write.writerows(fruit_list[:2])
+    write.writerows(appleA_valid)
+with open('image_test.csv', 'w') as f:
+    write = csv.writer(f)
+    for fruit_list in file_list:
+        write.writerows(fruit_list[-5:])
+    write.writerows(appleA_valid[-15:])
+
